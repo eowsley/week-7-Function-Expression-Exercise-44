83f62b6e2336245c2f374a29ac537d2f265fef58
eowsley <66394238+eowsley@users.noreply.github.com>
2020-10-29 17:50:09 -0700

    final commit

---
+++ index.js
@@ -0,0 +1,6 @@
+/* function that returns the square of a number. define the function as a 
+function expression, stored in a variable called square. */
+
+let mathSquare = square => square * square;
+
+console.log(mathSquare(3))
